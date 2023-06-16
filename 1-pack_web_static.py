b903bc2e730cab7d54fb9bc4a8ede9c3d1c9c7e6
Joseph Otine <117803219+joeotine@users.noreply.github.com>
2023-06-16 11:53:44 +0300

    AirBnB clone - MySQL all tasks done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
+This fabric script generates a tgz archive from the contents of the web_static
 folder of the AirBnB Clone repo
 """
 
