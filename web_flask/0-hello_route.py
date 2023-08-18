788b4dbe311e27398cd2309e85af64330e2083a8
Joseph Otine <joeotine@gmail.com>
2023-08-18 07:15:05 +0000

    Application server all tasks updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,11 @@ starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/airbnb-onepage/', strict_slashes=False)  # Updated route
+
+@app.route('airbnb_onepage', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Removed single quotes around port value
+    app.run(host='0.0.0.0', port='5000'
